f63a1ec7dc7da24d399d64423afcd526927f5336
Swapnil <81177774+Now-Tiger@users.noreply.github.com>
2022-12-04 19:26:34 +0530

    product matching

--- Shack Labs/scripts/assignment_two.py
+++ Shack Labs/scripts/assignment_two.py
@@ -34,13 +34,14 @@ def get_lowercase_text(column: str, data: pd.DataFrame) -> None:
         print(f"empty dataset: {len(data)}")
     
     data[column] = data[column].apply(lambda x: " ".join(x.lower() for x in x.split()))
-    
     return
+
 
 def merge_data(data_one: pd.DataFrame, data_two: pd.DataFrame) -> pd.DataFrame:
     """ returns a combined dataset from two datasets.
         Here I'm combining datasets for the product comparison.
-        Adding one extra column for own purpose to see the retail price difference """
+        Adding one extra column for own purpose to see the retail price difference 
+    """
     
     new_data = data_one.copy()
     new_data['filpk_product'] = data_two['product_name']
@@ -60,7 +61,6 @@ def fetch_product(product_name: str, data: pd.DataFrame, column_name: str) -> pd
     if column_name not in data.columns:
         print(f"{column_name} not available in the dataset")
         return
-
     return data[data[column_name].str.contains(product_name)]
 
 
@@ -90,25 +90,26 @@ def main() -> None:
     combined_data = merge_data(amazon, flipkart)
 
     print(fetch_product("women's", combined_data, 'amz_product'))
-
     return
 
 
 if __name__ == "__main__":
     main()
 
+
 # -- output --
 
 # (base) D:\>python assesment_two.py
-#                                              amz_product  amz_retail_price  ...  flipk_discounted_price retail_diff
-# 0                    alisha solid women's cycling shorts               982  ...                   379.0        17.0
-# 3                    alisha solid women's cycling shorts               694  ...                   267.0         5.0
-# 6                    alisha solid women's cycling shorts              1198  ...                   479.0         1.0
-# 9                    alisha solid women's cycling shorts              1197  ...                   479.0         2.0
-# 11                                carrel printed women's              2281  ...                   910.0        18.0
-# ...                                                  ...               ...  ...                     ...         ...
-# 19786                  my addiction women's a-line dress              1293  ...                   779.0         6.0
-# 19787                       indibox women's a-line dress              1483  ...                   999.0        16.0
-# 19788                         jiiah women's sheath dress              1043  ...                   734.0         6.0
-# 19812  lord's red women's slingback peeptoes women heels              1280  ...                   650.0        19.0
-# 19881  lord's antique gold women's peeptoe heels wome...              1634  ...                  1650.0        16.0
+# 0                    alisha solid women's cycling shorts               982                   438                alisha solid women's cycling shorts               999.0                   379.0         17.0
+# 3                    alisha solid women's cycling shorts               694                   325                alisha solid women's cycling shorts               699.0                   267.0          5.0
+# 6                    alisha solid women's cycling shorts              1198                   602                alisha solid women's cycling shorts              1199.0                   479.0          1.0
+# 9                    alisha solid women's cycling shorts              1197                   542                alisha solid women's cycling shorts              1199.0                   479.0          2.0
+# 11                                carrel printed women's              2281                  1080                             carrel printed women's              2299.0                   910.0         18.0
+# ...                                                  ...               ...                   ...                                                ...                 ...                     ...          ...
+# 19786                  my addiction women's a-line dress              1293                   934                  my addiction women's a-line dress              1299.0                   779.0          6.0
+# 19787                       indibox women's a-line dress              1483                  1136                       indibox women's a-line dress              1499.0                   999.0         16.0
+# 19788                         jiiah women's sheath dress              1043                   904                         jiiah women's sheath dress              1049.0                   734.0          6.0
+# 19812  lord's red women's slingback peeptoes women heels              1280                   784  lord's red women's slingback peeptoes women heels              1299.0                   650.0         19.0
+# 19881  lord's antique gold women's peeptoe heels wome...              1634                  2001  lord's antique gold women's peeptoe heels wome...              1650.0                  1650.0         16.0
+
+[3789 rows x 7 columns]
